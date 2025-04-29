77f685617ac621886ec0345916fdc2d50f53126c
Mateusz Pawluk <pawluk1988@icloud.com>
2025-04-29 17:42:35 +0200

    Dodano licznik odwiedzin oraz zaktualizowano nawigację w celu lepszego wyświetlania informacji

---
+++ counter.js
@@ -0,0 +1,21 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyBC9o4M9QJiYCj0OH0WholvMIBMZfEfA-k",
+    authDomain: "idontwanttoinventaname.firebaseapp.com",
+    databaseURL: "https://idontwanttoinventaname-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "idontwanttoinventaname",
+    storageBucket: "idontwanttoinventaname.firebasestorage.app",
+    messagingSenderId: "110138784983",
+    appId: "1:110138784983:web:3fbfbc311fb4c123fcc492"
+  };
+  
+    firebase.initializeApp(firebaseConfig);
+    const db = firebase.database();
+    const counterRef = db.ref('visits');
+  
+    counterRef.transaction(current => {
+      return (current || 0) + 1;
+    });
+  
+    counterRef.on('value', snapshot => {
+      document.getElementById('visitCounter').textContent = 'Odwiedzin: ' + snapshot.val();
+    });
