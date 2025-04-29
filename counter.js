c79e805793880185af6b39fac09b37e1e31383eb
Mateusz Pawluk <pawluk1988@icloud.com>
2025-04-29 17:48:07 +0200

    Zmieniono formatowanie kodu w pliku counter.js dla lepszej czytelności

--- counter.js
+++ counter.js
@@ -8,14 +8,21 @@
     appId: "1:110138784983:web:3fbfbc311fb4c123fcc492"
   };
   
-    firebase.initializeApp(firebaseConfig);
-    const db = firebase.database();
-    const counterRef = db.ref('visits');
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.database();
+  const counterRef = db.ref('visits');
   
+  const hasVisited = localStorage.getItem('visited');
+  
+  if (!hasVisited) {
     counterRef.transaction(current => {
       return (current || 0) + 1;
     });
   
-    counterRef.on('value', snapshot => {
-      document.getElementById('visitCounter').textContent = 'Odwiedzin: ' + snapshot.val();
-    });
+    localStorage.setItem('visited', 'true');
+  }
+  
+  counterRef.on('value', snapshot => {
+    document.getElementById('visitCounter').textContent = 'Odwiedzin: ' + snapshot.val();
+  });
+  
